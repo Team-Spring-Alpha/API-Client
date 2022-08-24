d74b7a203e056ae3610add6f8ca7423ea79a0ef3
Heloisa Hobold Barbosa <heloisa.barbosa.pb@compasso.com.br>
2022-08-24 14:10:59 -0400

    fix busca por gênero

--- search/src/main/java/br/com/compass/search/controller/SearchController.java
+++ search/src/main/java/br/com/compass/search/controller/SearchController.java
@@ -39,10 +39,4 @@ public class SearchController {
         List<ResponseApiClient> responseApiClientList = searchService.findByGenre(movieGenre.getIdGenrer());
         return ResponseEntity.ok(responseApiClientList);
     }
-
-    @GetMapping("/movie-actor")
-    public ResponseEntity<List<ResponseApiClient>> getMovieByActor(@RequestParam String movieActor) {
-        List<ResponseApiClient> responseApiClientList = searchService.findByActor(movieActor);
-        return ResponseEntity.ok(responseApiClientList);
-    }
 }
