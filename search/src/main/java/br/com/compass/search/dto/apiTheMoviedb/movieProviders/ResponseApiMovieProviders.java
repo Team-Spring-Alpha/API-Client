2967cbbac6110cde21809ac9e07066d8a0be35cc
PedroAS <85406215+PedroHAlvesS@users.noreply.github.com>
2022-08-26 12:33:04 -0300

    remove search api

--- search/src/main/java/br/com/compass/search/dto/apiTheMoviedb/movieProviders/ResponseApiMovieProviders.java
+++
@@ -1,9 +0,0 @@
-package br.com.compass.search.dto.apiTheMoviedb.movieProviders;
-
-import lombok.Data;
-
-@Data
-public class ResponseApiMovieProviders {
-    private Long id;
-    private ResponseApiMovieProvidersResults results;
-}
--- search/src/main/java/br/com/compass/search/dto/apiclient/response/ResponseRentAndBuy.java
+++
@@ -1,9 +0,0 @@
-package br.com.compass.search.dto.apiclient.response;
-
-import lombok.Data;
-
-@Data
-public class ResponseRentAndBuy {
-    private String store;
-    private Double price;
-}
