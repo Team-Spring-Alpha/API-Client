a9950f0b36d083c226d783c1b1fedd0cd9cba7de
Heloisa Hobold Barbosa <heloisa.barbosa.pb@compasso.com.br>
2022-09-07 08:13:22 -0400

    adicionando try catch ao proxy da history

--- src/main/java/br/com/compass/filmes/user/client/AllocationHistoryProxy.java
+++ src/main/java/br/com/compass/filmes/user/client/AllocationHistoryProxy.java
@@ -1,8 +1,11 @@
 package br.com.compass.filmes.user.client;
 
 import br.com.compass.filmes.user.dto.user.response.apiAllocationHistory.ResponseAllocationDTO;
+import feign.FeignException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -13,6 +16,10 @@ public class AllocationHistoryProxy {
     private AllocationHistory allocationHistory;
 
     public List<ResponseAllocationDTO> getHistoryByUser(String userId) {
-        return allocationHistory.getHistoryByUser(userId);
+        try {
+            return allocationHistory.getHistoryByUser(userId);
+        } catch (FeignException.FeignServerException.NotFound exception) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
     }
 }
